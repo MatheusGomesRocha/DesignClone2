c8102047be5ef7209444e65dc872b876f1483a83
Matheus Gomes <matheusgomes192@hotmail.com>
2021-02-15 11:58:44 -0300

    Finalizando front-end do Component More + adicionando icons para usar nesse component + adicionando imagem para usar no Component Comments

--- src/components/More.js
+++ src/components/More.js
@@ -6,75 +6,45 @@ import Item3 from '../img/item3.jpg';
 import Item4 from '../img/item4.jpg';
 import Item5 from '../img/item5.jpg';
 import Item6 from '../img/item6.jpg';
-import {useState} from "react";
 
+import TelescopeIcon from '../svg/telescope';
+import ReactIcon from '../svg/physics';
+import CompassIcon from '../svg/compass';
+
+let array = [
+    {id: 1, img: Item2, title: 'Rewing Watch App', subTitle: 'Branding, Graphic'},
+    {id: 2, img: Item4, title: 'Experience Pad UI', subTitle: 'Web-design, Graphic'},
+    {id: 3, img: Item1, title: 'PlayIt Disk Cover', subTitle: 'Print, Branding'},
+    {id: 4, img: Item5, title: 'Sketching Fun', subTitle: 'Print, Branding'},
+    {id: 5, img: Item6, title: 'Fabric Bag', subTitle: 'Print, Branding'},
+    {id: 6, img: Item3, title: 'Deer Wildfile', subTitle: 'Print, Branding'},
+]
 export default () => {
-    const [hover1, setHover1] = useState(false);
-    const [hover2, setHover2] = useState(false);
-    const [hover3, setHover3] = useState(false);
-    const [hover4, setHover4] = useState(false);
-    const [hover5, setHover5] = useState(false);
-    const [hover6, setHover6] = useState(false);
 
     return (
         <div className={"more"}>
             <div className={"gridDiv"}>
-
-                <div className={"moreItem"}>
-                    <img className={"img"} src={Item2}/>
-                    <div className={"imgHidden"}>
-                        <span className={"title"}>Rewind Watch App</span>
-                        <span className={"subTitle"}>Branding, Graphic</span>
+                {array.map((item, k) => (
+                    <div key={k} className={"moreItem"}>
+                        <img className={"img"} src={item.img}/>
+                        <div className={"imgHidden"}>
+                            <span className={"title"}>{item.title}</span>
+                            <span className={"subTitle"}>{item.subTitle}</span>
+                        </div>
                     </div>
-                </div>
 
-                <div className={"moreItem"}>
-                    <img className={"img"} src={Item4}/>
-                    <div className={"imgHidden"}>
-                        <span className={"title"}>Experience Pad UI</span>
-                        <span className={"subTitle"}>Web-design, Graphic</span>
-                    </div>
-                </div>
-
-                <div className={"moreItem"}>
-                    <img className={"img"} src={Item1}/>
-                    <div className={"imgHidden"}>
-                        <span className={"title"}>PlayIt Disk Cover</span>
-                        <span className={"subTitle"}>Print, Branding</span>
-                    </div>
-                </div>
-
-                <div className={"moreItem"}>
-                    <img className={"img"} src={Item5}/>
-                    <div className={"imgHidden"}>
-                        <span className={"title"}>Sketching Fun</span>
-                        <span className={"subTitle"}>Print, Branding</span>
-                    </div>
-                </div>
-
-                <div className={"moreItem"}>
-                    <img className={"img"} src={Item6}/>
-                    <div className={"imgHidden"}>
-                        <span className={"title"}>Fabric Bag</span>
-                        <span className={"subTitle"}>Print, Graphic</span>
-                    </div>
-                </div>
-
-                <div className={"moreItem"}>
-                    <img className={"img"} src={Item3}/>
-                    <div className={"imgHidden"}>
-                        <span className={"title"}>Deer Wildfile</span>
-                        <span className={"subTitle"}>Print, Branding</span>
-                    </div>
-                </div>
+                ))}
             </div>
 
             <div className={"bottomDiv"}>
                 <div className={"bottomItemDiv"}>
+
                     <div className={"bottomLeftDiv"}>
-                        AAA
+                        <TelescopeIcon fill={"#0CB4CE"}/>
                     </div>
+
                     <div className={"bottomRightDiv"}>
+
                         <span className={"title"}>Stunning Design</span>
                         <span className={"subTitle"}>
                             Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
@@ -85,7 +55,7 @@ export default () => {
 
                 <div className={"bottomItemDiv"}>
                     <div className={"bottomLeftDiv"}>
-                        AAA
+                        <CompassIcon/>
                     </div>
                     <div className={"bottomRightDiv"}>
                         <span className={"title"}>Retina Optimized</span>
@@ -98,7 +68,7 @@ export default () => {
 
                 <div className={"bottomItemDiv"}>
                     <div className={"bottomLeftDiv"}>
-                        AAA
+                        <ReactIcon/>
                     </div>
                     <div className={"bottomRightDiv"}>
                         <span className={"title"}>Fast & Smooth</span>
